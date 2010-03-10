14d30945b814c47432ce2a7c699d90d8f12fa2e5
Martin Kamerbeek <martin@oqapi.nl>
2010-03-10 18:05:10 +0100

    Better POD.

--- lib/MooseX/SlaveAttribute.pm
+++ lib/MooseX/SlaveAttribute.pm
@@ -3,6 +3,8 @@ package MooseX::SlaveAttribute;
 use warnings;
 use strict;
 
+our $VERSION = '0.1';
+
 #--------------------------------------------------------
 package MooseX::SlaveAttribute;
 use Moose::Role;
@@ -13,6 +15,7 @@ has master => (
     predicate   => 'has_master_attr',
 );
 
+# Warn that slave traits and default values don't mix.
 before install_accessors => sub {
     my $self = shift;
     
@@ -68,19 +71,99 @@ MooseX::SlaveAttribute - Let your attributes default to the value of a master at
 
 Version 0.1
 
-=cut
-
-our $VERSION = '0.1';
-
 =head1 SYNOPSIS
 
+    {
+        package Foo;
+        use Moose;
+        use MooseX::SlaveAttribute;
 
-Perhaps a little code snippet.
+        has color           => ( is => 'rw', default => 'red' );
+        has line_color      => ( is => 'rw', traits => ['Slave'], master => 'color' );
+        has stroke_color    => ( is => 'rw', traits => ['Slave'], master => 'line_color' );
+    }
 
+    my $f = Foo->new;
+
+    $f->line_color;     # 'red'
+    $f->stroke_color;   # 'red'
+
+    $f->color( 'blue' );
+    $f->line_color;     # 'blue'
+    $f->stroke_color;   # 'blue'
+
+    $f->line_color( 'green' );
+    $f->line_color      # 'green'
+    $f->stroke_color    # 'green'
+
+=head1 DESCRIPTION
+
+MooseX::SlaveAttribute allows you to let your attributes default to the values of other attributes. This can be
+very handy if you have a lot of attributes that in most cases you want to default to some settable value, while
+still being able to give them a custom value if necessary.
+
+If you explicitly assign a value to a slave attribute it will stop following the value of its master.
+
+In order to create a slave attribute give it the 'Slave' trait and pass the name of the master attribute via the
+'master' property, like this:
+
+    has foo => (
+        is      => 'rw',
+        default => 'FOO',
+    );
+
+    has bar => (
+        is      => 'rw',
+        traits  => [ 'Slave' ],
+        master  => 'foo',
+    );
+
+Slave attributes can have masters that are are slave attributes themselves, like this:
+
+    has foo => (
+        is      => 'rw',
+        default => 'FOO',
+    );
+
+    has bar => (
+        is      => 'rw',
+        traits  => [ 'Slave' ],
+        master  => 'foo',
+    );
+
+    has baz => (
+        is      => 'rw'
+        traits  => [ 'Slave' ],
+        master  => 'bar',
+    );
+    
+
+Masters can also be attributes of a super class:
+
+    package Foo;
+    use Moose;
+
+    has xyzzy => (
+        is      => 'rw',
+        default => 'baz',
+    }
+
+    package Bar;
+    use Moose;
     use MooseX::SlaveAttribute;
 
-    my $foo = MooseX::SlaveAttribute->new();
-    ...
+    extends 'Foo';
+
+    has zazzy => (
+        is      => 'rw',
+        traits  => [ 'Slave' ],
+        master  => 'xyzzy',
+    );
+
+=head1 CAVEATS
+
+Don't give slave attributes a default of their own. If you do, your attribute will not be a slave to its master.
+You'll get a warning when you load you class, when you do this, though.
 
 =head1 AUTHOR
 
@@ -89,15 +172,14 @@ Martin Kamerbeek, C<< <mhg at cpan.org> >>
 =head1 BUGS
 
 Please report any bugs or feature requests to C<bug-moosex-slaveattribute at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-SlaveAttribute>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-SlaveAttribute>. It will help greatly
+if you attach a small program that reproduces the bug.
 
 =head1 SUPPORT
 
 You can find documentation for this module with the perldoc command.
 
     perldoc MooseX::SlaveAttribute
-
 
 You can also look for information at:
 
@@ -120,10 +202,6 @@ L<http://cpanratings.perl.org/d/MooseX-SlaveAttribute>
 L<http://search.cpan.org/dist/MooseX-SlaveAttribute/>
 
 =back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 COPYRIGHT & LICENSE
 
